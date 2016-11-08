f24a7acc27e8fcdc9f2a6a02647162570b0c931d
kieranhj <kieran@kconnell.co.uk>
2016-11-08 18:59:29 +0000

    Font glyph / sprite sheet conversion
    Found some nice fonts and example sprites.
    Added ability to extract font glyph / sprite sheets that are fixed sized
    areas from a larger image.
    Some compilation options for enabling double buffering & screen clear in
    6502.

--- mode7-sprites/mode7-sprites.cpp
+++ mode7-sprites/mode7-sprites.cpp
@@ -8,9 +8,15 @@ using namespace cimg_library;
 
 static CImg<unsigned char> src;
 
+static int global_x = 0;
+static int global_y = 0;
+static int global_w = 0;
+static int global_h = 0;
+
 int get_colour_from_rgb(unsigned char r, unsigned char g, unsigned char b)
 {
-	return (r ? 1 : 0) + (g ? 2 : 0) + (b ? 4 : 0);
+//	return (r ? 1 : 0) + (g ? 2 : 0) + (b ? 4 : 0);
+	return r ? 7 : (g ? 7 : (b ? 7 : 0));
 }
 
 int get_pixel_from_image(int x, int y, int x_offset, int y_offset)
@@ -18,10 +24,15 @@ int get_pixel_from_image(int x, int y, int x_offset, int y_offset)
 	x -= x_offset;
 	y -= y_offset;
 
-	if (x < 0 || x >= src._width) return 0;
-	if (y < 0 || y >= src._height) return 0;
+	if (x < 0 || x >= global_w) return 0;
+	if (y < 0 || y >= global_h) return 0;
 
-	return get_colour_from_rgb(src(x, y, 0), src(x, y, 1), src(x, y, 2));
+	x += global_x;
+	y += global_y;
+
+//	printf("(%d, %d) = [%d %d %d]\n", x, y, src(x, y, 0), src(x, y, 1), src(x, y, 2));
+
+	return get_colour_from_rgb(src(x, y, 0), src._depth>1?src(x, y, 1):src(x,y,0), src._depth>2?src(x, y, 2):src(x,y,0));
 }
 
 unsigned char get_graphic_char_from_image(int x7, int y7, int x_offset, int y_offset)
@@ -58,6 +69,62 @@ unsigned char get_mask_char_from_image(int x7, int y7, int x_offset, int y_offse
 		+ (get_pixel_from_image(x + 1, y + 2, x_offset, y_offset) ? 64 : 0);
 }
 
+void make_sprite_data(FILE *outfile, const char *label, int pixel_width, int pixel_height, bool swizzle)
+{
+	fprintf(outfile, ".%s_data\n", label);
+
+	if (swizzle)
+	{
+		for (int x = 0; x < pixel_width; x++)
+		{
+			fprintf(outfile, "EQUB ");
+
+			for (int y = -2; y < pixel_height - 2; y += 3)
+			{
+				if (y != -2)
+				{
+					fprintf(outfile, ",");
+				}
+
+				unsigned char pixels = (get_pixel_from_image(x, y, 0, 0) == 7 ? 16 : 0)
+					+ (get_pixel_from_image(x, y + 1, 0, 0) == 7 ? 8 : 0)
+					+ (get_pixel_from_image(x, y + 2, 0, 0) == 7 ? 4 : 0)
+					+ (get_pixel_from_image(x, y + 3, 0, 0) == 7 ? 2 : 0)
+					+ (get_pixel_from_image(x, y + 4, 0, 0) == 7 ? 1 : 0);
+
+				fprintf(outfile, "%d", pixels);
+			}
+
+			fprintf(outfile, "\n");
+		}
+	}
+	else
+	{
+		for (int y = -2; y < pixel_height - 2; y += 3)
+		{
+			fprintf(outfile, "EQUB ");
+
+			for (int x = 0; x < pixel_width; x++)
+			{
+				if (x != 0)
+				{
+					fprintf(outfile, ",");
+				}
+
+				unsigned char pixels = (get_pixel_from_image(x, y, 0, 0) == 7 ? 16 : 0)
+					+ (get_pixel_from_image(x, y + 1, 0, 0) == 7 ? 8 : 0)
+					+ (get_pixel_from_image(x, y + 2, 0, 0) == 7 ? 4 : 0)
+					+ (get_pixel_from_image(x, y + 3, 0, 0) == 7 ? 2 : 0)
+					+ (get_pixel_from_image(x, y + 4, 0, 0) == 7 ? 1 : 0);
+
+				fprintf(outfile, "%d", pixels);
+			}
+
+			fprintf(outfile, "\n");
+		}
+	}
+}
+
 int main(int argc, char **argv)
 {
 	cimg_usage("MODE 7 sprite convertor.\n\nUsage : mode7-sprites [options]");
@@ -66,6 +133,10 @@ int main(int argc, char **argv)
 	const char *label = cimg_option("-label", (char*)0, "Label");
 	const char *output_name = cimg_option("-o", (char*)0, "Output filename");
 	const bool mask = cimg_option("-mask", false, "Add mask data to output");
+	const bool font = cimg_option("-font", false, "Extract font glyphs / sprite sheet");
+	const char *const geom = cimg_option("-g", "16x16", "Sprite input size (when extracting from font/sprite sheet)");
+	const int num_glyphs = cimg_option("-n", 0, "Num glyphs / sprites to extract (0=all)");
+	const bool swizzle = cimg_option("-swizzle", false, "Output data in column order (default=row order)");
 
 	if (cimg_option("-h", false, 0)) std::exit(0);
 	if (input_name == NULL)  std::exit(0);
@@ -85,47 +156,83 @@ int main(int argc, char **argv)
 	printf("Image=%d x %d\n", src._width, src._height);
 
 	int pixel_width = src._width;
-	if (pad) pixel_width++;
+	int pixel_height = src._height;
+
+	if (font)
+	{
+		std::sscanf(geom, "%d%*c%d", &pixel_width, &pixel_height);
+	}
+	else
+	{
+		if (pad) pixel_width++;
+		if (pad) pixel_height += 2;
+	}
+
+	global_w = pixel_width;
+	global_h = pixel_height;
+
 	int char_width = pixel_width / 2;
 	if (pixel_width % 2) char_width++;
-
-	int pixel_height = src._height;
-	if (pad) pixel_height += 2;
 	int char_height = pixel_height / 3;
-	if (pixel_height % 3) char_height += 3 - (pixel_height % 3);
+	if (pixel_height % 3) char_height++;
 
 	printf("Pixels=%d x %d\n", pixel_width, pixel_height);
 	printf("Chars=%d x %d\n", char_width, char_height);
 
 	fprintf(outfile, "\\\\ Input file '%s'\n", input_name);
 	fprintf(outfile, "\\\\ Image size=%dx%d pixels=%dx%d\n", src._width, src._height, pixel_width, pixel_height);
+	fprintf(outfile, "\\\\ Data in %s order\n", swizzle ? "COLUMN" : "ROW");
 	fprintf(outfile, ".%s\n", label);
 
-	
-	fprintf(outfile, "EQUB %d, %d\t;pixel width, char height\n", pixel_width, char_height);
-	fprintf(outfile, ".%s_data\n", label);
-
-	for (int y = -2; y < pixel_height - 2; y += 3)
+	if (swizzle)
 	{
-		fprintf(outfile, "EQUB ");
+		fprintf(outfile, "EQUB %d, %d\t;char height, pixel width\n", char_height, pixel_width);
+	}
+	else
+	{
+		fprintf(outfile, "EQUB %d, %d\t;pixel width, char height\n", pixel_width, char_height);
+	}
 
-		for (int x = 0; x < pixel_width; x++)
+	if (font)
+	{
+		int max = (src._height / pixel_height)*(src._width / pixel_width);
+
+		if (num_glyphs && max > num_glyphs)
 		{
-			if (x != 0)
-			{
-				fprintf(outfile, ",");
-			}
-
-			unsigned char pixels = (get_pixel_from_image(x, y, 0, 0) == 7 ? 16 : 0)
-				+ (get_pixel_from_image(x, y + 1, 0, 0) == 7 ? 8 : 0)
-				+ (get_pixel_from_image(x, y + 2, 0, 0) == 7 ? 4 : 0)
-				+ (get_pixel_from_image(x, y + 3, 0, 0) == 7 ? 2 : 0)
-				+ (get_pixel_from_image(x, y + 4, 0, 0) == 7 ? 1 : 0);
-
-			fprintf(outfile, "%d", pixels);
+			max = num_glyphs;
 		}
 
-		fprintf(outfile, "\n");
+		fprintf(outfile, ".%s_table_LO\n", label);
+		for (int i = 0; i < max; i++)
+		{
+			fprintf(outfile, "EQUB LO(%s%d_data)\n", label, i);
+		}
+		fprintf(outfile, ".%s_table_HI\n", label);
+		for (int i = 0; i < max; i++)
+		{
+			fprintf(outfile, "EQUB HI(%s%d_data)\n", label, i);
+		}
+
+		int n = 0;
+		for (int gy = 0; gy < src._height; gy += pixel_height)
+		{
+			for (int gx = 0; gx < src._width && n < max; gx += pixel_width, n++)
+			{
+				global_x = gx;
+				global_y = gy;
+
+				char gl[256];
+				sprintf(gl, "%s%d", label, n);
+
+				printf("%d at (%d, %d)\n", n, gx, gy);
+
+				make_sprite_data(outfile, gl, pixel_width, pixel_height, swizzle);
+			}
+		}
+	}
+	else
+	{													
+		make_sprite_data(outfile, label, pixel_width, pixel_height, swizzle);
 	}
 
 	fclose(outfile);
